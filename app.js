31de27cfba93a9273e847bf6e3c9693cf1bb1bdf
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-10-29 15:21:35 -0400

    added some css and also the animals shows which state they are located in

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const ejs = require("ejs");
 const https = require("https");
 const favicon = require("express-favicon");
 const petfinder = require("@petfinder/petfinder-js");
-const keys = require(__dirname + "/keys.js")
-const getAnimalData = require(__dirname + "/function.js")
+const keys = require(__dirname + "/keys.js");
+const getAnimalData = require(__dirname + "/function.js");
 
-const apiKey = keys.apiKey()
-const secretKey = keys.secretKey()
+const apiKey = keys.apiKey();
+const secretKey = keys.secretKey();
 const client = new petfinder.Client({ apiKey: apiKey, secret: secretKey });
 
 const app = express();
@@ -20,45 +20,71 @@ app.use(express.static("public"));
 app.use(favicon(__dirname + "/public/images/paw.png"));
 
 let animalsForAdoption = [];
+let pageNumber = 0;
+let zipCode;
 
 app.get("/", function (req, res) {
+  pageNumber = 0;
   async function getAnimal() {
     await client.animal
       .search({
         limit: 3,
       })
       .then((resp) => {
-        getAnimalData.storeAnimalData(resp, animalsForAdoption)
+        getAnimalData.storeAnimalData(resp, animalsForAdoption);
       })
       .then(() => {
-          console.log(animalsForAdoption)
-        res.render("home", {animals: animalsForAdoption});
-        animalsForAdoption = []
+        // console.log(animalsForAdoption);
+        // console.log(animalsForAdoption.length);
+        res.render("home", { animals: animalsForAdoption });
+        animalsForAdoption = [];
       });
   }
   getAnimal();
 });
 
 app.post("/findAPet", function (req, res) {
-    console.log(req.body);
-  const zipCode = req.body.location;
+    if(req.body.previous === "") {
+        pageNumber--;
+    } else{
+        pageNumber++;
+    }
+    if(req.body.location) {
+        zipCode = req.body.location;
+    }
+  console.log(zipCode);
   async function getAnimal() {
     await client.animal
       .search({
+        page: pageNumber,
         limit: 20,
-        location: zipCode
+        location: zipCode,
       })
       .then((resp) => {
-        getAnimalData.storeAnimalData(resp, animalsForAdoption)
+        getAnimalData.storeAnimalData(resp, animalsForAdoption);
       })
       .then(() => {
-          console.log(animalsForAdoption)
-        res.render("animal", {animals: animalsForAdoption});
-        animalsForAdoption = []
+        //   console.log(animalsForAdoption[0]);
+        res.render("animal", { animals: animalsForAdoption, pagenumber: pageNumber });
+        animalsForAdoption = [];
       });
   }
   getAnimal();
 });
+
+app.post("/adopt", function(req,res) {
+    const selectedAnimal = req.body.id;
+    async function getAnimal() {
+        await client.animal
+          .show(selectedAnimal)
+          .then((resp) => {
+            //   console.log(resp.data.animal);
+            const animalData = resp.data.animal
+            res.render("adopt", { selectedAnimalData: animalData});
+          })
+      }
+      getAnimal();
+})
 
 app.listen(3000, function (req, res) {
   console.log("APP listening on PORT 3000");
--- function.js
+++ function.js
@@ -1,19 +1,22 @@
-
 exports.storeAnimalData = function (resp, arr) {
-    const animals = resp.data.animals;
-        animals.forEach((data) => {
-          const animal = {
-            id: data.id,
-            name: data.name,
-            url: data.url,
-            breed: data.breeds,
-            age: data.age,
-            sex: data.gender,
-            size: data.size,
-            desc: data.description,
-            contact: data.contact,
-            photo: data.photos,
-          };
-          arr.push(animal);
-        });
-}
+  const animals = resp.data.animals;
+  animals.forEach((data) => {
+    if (data.photos[0] === undefined) {
+      console.log("undefined");
+    } else {
+      const animal = {
+        id: data.id,
+        name: data.name,
+        url: data.url,
+        breed: data.breeds,
+        age: data.age,
+        sex: data.gender,
+        size: data.size,
+        desc: data.description,
+        contact: data.contact,
+        photo: data.photos,
+      };
+      arr.push(animal);
+    }
+  });
+};
