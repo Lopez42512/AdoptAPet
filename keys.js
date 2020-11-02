db76ad08d7fdff993fac2010a8b61063985864d7
Ismael Lopez <ismaellopez@Ismaels-MacBook-Pro.local>
2020-11-02 16:09:55 -0500

    finish the home page so the user can search between cat, dogs or both also added some css

--- keys.js
+++
@@ -1,7 +0,0 @@
-exports.apiKey = function() {
-    return "ZXjPUajg5LXVxKmoF98wSO4kfBtPQ1RovmuIY7X6T8cQ3BkniT"
-}
-
-exports.secretKey = function() {
-    return "5cstTZJ7K6P9v6O1JommJyPSsD6GaZCJFus4dILl"
-}
